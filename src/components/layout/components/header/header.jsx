c7fbe5eff79670a2daf33957867eb5824c93e2de
Саша Егорова <aleksa.smile@icloud.com>
2025-02-12 17:41:54 +0300

    moved from src/components  to components in layout

---
+++ src/components/layout/components/header/header.jsx
@@ -0,0 +1,20 @@
+import { Container } from '@/components/container/index';
+import { Navbar } from '@/components/layout/components/header/components/navbar';
+import { Logo } from '@/components/layout/components/logo';
+import { PhoneCallButton } from '@/components/layout/components/phone-call-button';
+
+import styles from './header.module.scss';
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Container isWide>
+        <div className={styles.content}>
+          <Logo />
+          <Navbar />
+          <PhoneCallButton />
+        </div>
+      </Container>
+    </header>
+  );
+};
