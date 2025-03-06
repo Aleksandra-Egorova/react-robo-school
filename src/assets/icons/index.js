1a404e98a113bc65e1b702ecc222e652a062bd59
Саша Егорова <aleksa.smile@icloud.com>
2025-03-06 17:33:55 +0300

    added icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,8 @@
+import { ArrowDownIcon } from './ArrowDownIcon';
 import { BurgerIcon } from './BurgerIcon';
 import { CloseIcon } from './CloseIcon';
+import { CloseIconBlack } from './CloseIconBlack';
 import { PhoneIcon } from './PhoneIcon';
 import { TooltipIcon } from './TooltipIcon';
 
-export { BurgerIcon, CloseIcon, PhoneIcon, TooltipIcon };
+export { ArrowDownIcon, BurgerIcon, CloseIcon, CloseIconBlack, PhoneIcon, TooltipIcon };
