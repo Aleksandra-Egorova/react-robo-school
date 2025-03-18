07f52e7a81613c6eeb55c94d9af613a23099bbc9
Саша Егорова <aleksa.smile@icloud.com>
2025-03-18 12:53:52 +0300

    changed for desktop and mobile versions

--- src/components/layout/components/phone-call-button/phone-call-button.jsx
+++ src/components/layout/components/phone-call-button/phone-call-button.jsx
@@ -1,9 +1,22 @@
+import { PhoneIcon } from '@/assets/icons';
+import { useWindowSize } from '@/hooks/useWindowSize';
+
 import styles from './phone-call-button.module.scss';
 
 export const PhoneCallButton = () => {
+  const { width } = useWindowSize();
+
   return (
-    <a href="tel:88000001122" className={styles.phoneCall}>
-      +7 800 000 11 22
-    </a>
+    <>
+      {width <= 1024 ? (
+        <a href="tel:88000001122" className={styles.mobilePhoneCall}>
+          <PhoneIcon />
+        </a>
+      ) : (
+        <a href="tel:88000001122" className={styles.desktopPhoneCall}>
+          +7 800 000 11 22
+        </a>
+      )}
+    </>
   );
 };
