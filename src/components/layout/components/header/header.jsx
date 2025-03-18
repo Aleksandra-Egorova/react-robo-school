de3a9214f462366a775df5ff2944421e1488c0a5
Саша Егорова <aleksa.smile@icloud.com>
2025-03-18 19:19:47 +0300

    changed Header component

--- src/components/layout/components/header/header.jsx
+++ src/components/layout/components/header/header.jsx
@@ -1,33 +1,17 @@
 import { Container } from '@/components/container/index';
-import { useWindowSize } from '@/hooks/useWindowSize';
 
 import { Logo } from '../logo';
-import { PhoneCallButton } from '../phone-call-button';
-import { DesktopNavbar } from './devices/desktop';
-import { MobileNavbar } from './devices/mobile';
+import { Navbar } from './components/navbar';
 
 import styles from './header.module.scss';
 
 export const Header = () => {
-  const { width } = useWindowSize();
-
   return (
     <header className={styles.header}>
       <Container isWide>
         <div className={styles.content}>
           <Logo />
-
-          {width <= 1024 ? (
-            <div className={styles.mobileContent}>
-              <MobileNavbar />
-              <PhoneCallButton />
-            </div>
-          ) : (
-            <>
-              <DesktopNavbar />
-              <PhoneCallButton />
-            </>
-          )}
+          <Navbar />
         </div>
       </Container>
     </header>
