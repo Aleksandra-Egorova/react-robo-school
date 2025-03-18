1d44c51169ded7f975fdf9b2aae28d774d344892
Саша Егорова <aleksa.smile@icloud.com>
2025-03-18 12:52:26 +0300

    added DesktopNavbar, MobileNavbar components, hook useWindowSize

--- src/components/layout/components/header/header.jsx
+++ src/components/layout/components/header/header.jsx
@@ -1,19 +1,33 @@
 import { Container } from '@/components/container/index';
+import { useWindowSize } from '@/hooks/useWindowSize';
 
 import { Logo } from '../logo';
 import { PhoneCallButton } from '../phone-call-button';
-import { Navbar } from './components/navbar';
+import { DesktopNavbar } from './devices/desktop';
+import { MobileNavbar } from './devices/mobile';
 
 import styles from './header.module.scss';
 
 export const Header = () => {
+  const { width } = useWindowSize();
+
   return (
     <header className={styles.header}>
       <Container isWide>
         <div className={styles.content}>
           <Logo />
-          <Navbar />
-          <PhoneCallButton />
+
+          {width <= 1024 ? (
+            <div className={styles.mobileContent}>
+              <MobileNavbar />
+              <PhoneCallButton />
+            </div>
+          ) : (
+            <>
+              <DesktopNavbar />
+              <PhoneCallButton />
+            </>
+          )}
         </div>
       </Container>
     </header>
